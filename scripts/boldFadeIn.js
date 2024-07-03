4f731197768aa394880e2349769a185830d21e35
Carolyne Wamono <cwamono@gmail.com>
2024-07-03 10:40:52 +0200

    kuwona

--- scripts/boldFadeIn.js
+++ scripts/boldFadeIn.js
@@ -1,5 +1,37 @@
+// document.addEventListener('DOMContentLoaded', function() {
+// const birdElements = document.querySelectorAll('.bird');
+
+//   function isInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//       rect.top >= 0 &&
+//       rect.left >= 0 &&
+//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+//   }
+
+//   function handleVisibility() {
+//     birdElements.forEach(function(bird) {
+//       if (isInViewport(bird) && !bird.classList.contains('bold')) {
+//         bird.style.transition = 'font-weight 3s ease'; // Apply transition when becoming bold
+//         bird.style.fontWeight = '500'; // Immediately change to bold
+//         setTimeout(function() {
+//           bird.classList.add('bold');
+//         }, 1000); // Delay adding the 'bold' class
+//       }
+//     });
+//   }
+
+//   // Initial check on page load
+//   handleVisibility();
+
+//   // Listen for scroll and resize events
+//   window.addEventListener('scroll', handleVisibility);
+//   window.addEventListener('resize', handleVisibility);
+// });
 document.addEventListener('DOMContentLoaded', function() {
-const birdElements = document.querySelectorAll('.bird');
+  const birdElements = document.querySelectorAll('.bird');
 
   function isInViewport(element) {
     const rect = element.getBoundingClientRect();
@@ -14,11 +46,7 @@ const birdElements = document.querySelectorAll('.bird');
   function handleVisibility() {
     birdElements.forEach(function(bird) {
       if (isInViewport(bird) && !bird.classList.contains('bold')) {
-        bird.style.transition = 'font-weight 3s ease'; // Apply transition when becoming bold
-        bird.style.fontWeight = '500'; // Immediately change to bold
-        setTimeout(function() {
-          bird.classList.add('bold');
-        }, 1000); // Delay adding the 'bold' class
+        bird.classList.add('bold');
       }
     });
   }
