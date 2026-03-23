c192d0c49904fb3f8e77f7f53682da3281faa612
MichaelKleynhans <kleynhansmic@gmail.com>
2026-03-23 20:03:34 +0200

    mobile menu3

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,4 +4,5 @@ const menu = document.querySelector('.site-navigation-list');
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('active');
   menuToggle.textContent = menu.classList.contains('active') ? 'close' : 'menu';
+  document.body.classList.toggle('menu-open');
 });
